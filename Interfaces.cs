46f18c9259dcbaacec12b9ec7c6d9ec632f0caad
ManvithMaddali <113430983+ManvithMaddali@users.noreply.github.com>
2022-11-17 16:39:21 +0530

    Add files via upload

---
+++ Interfaces.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Interfaces : MonoBehaviour
+{
+    public void Ak47()
+    {
+        Debug.Log("Is an Assault Rifle");
+        Debug.Log("Fires 7.62mm bullets");
+    }
+
+    public void pistol()
+    {
+        Debug.Log("Is an SMG");
+        Debug.Log("Fires 9mm bullets");
+    }
+    public virtual void Multiply(int x,int y)
+    {
+        Debug.Log(x + y);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
