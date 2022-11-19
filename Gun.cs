3c086bd4a91079fc9e8d263990b6a267502af02b
Manvith <maddalimanvith@gmail.com>
2022-11-19 18:44:58 +0530

    Removed Because Errors Are comming if we want we should write it in unity c# format not in text format

--- Gun.cs
+++ Gun.cs
@@ -6,13 +6,14 @@ public class Gun : MonoBehaviour
 {
     private RaycastHit hit;
     public GameObject bullet;
+    public float force;
     public GameObject barrel;
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
            GameObject instaBullet= Instantiate(bullet, barrel.transform.position, barrel.transform.rotation);
-            instaBullet.GetComponent<Rigidbody>().AddForce(Vector3.back * 500);
+            instaBullet.GetComponent<Rigidbody>().AddForce(Vector3.back * force);
             Destroy(instaBullet, 12f);
         }
     }
